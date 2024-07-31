d71c10a208608d5353a31b8c21c67fcf1d4c6de5
ma5d <2689169757@qq.com>
2024-07-31 18:34:30 +0800

    init

---
+++ src/main/java/org/ma5d/javamock/JavaMockApplication.java
@@ -0,0 +1,13 @@
+package org.ma5d.javamock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ma5d/javamock/JavaMockApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ma5d.javamock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
