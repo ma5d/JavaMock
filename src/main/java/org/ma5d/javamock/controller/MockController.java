4d1d88b73743f5dadbfbf321de929c5a8f727a86
ma5d <2689169757@qq.com>
2024-08-03 16:56:15 +0800

    save detail finish

--- src/main/java/org/ma5d/javamock/controller/MockController.java
+++ src/main/java/org/ma5d/javamock/controller/MockController.java
@@ -5,10 +5,7 @@ import org.ma5d.javamock.dto.SaveParam;
 import org.ma5d.javamock.service.JavaMockServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -18,15 +15,24 @@ public class MockController {
     @Resource
     JavaMockServiceImpl javaMockService;
 
-    @PostMapping("/save")
-    public Boolean configure(@ModelAttribute("saveParam") SaveParam saveParam) throws SQLException {
-        return javaMockService.saveConfig(saveParam);
-    }
-
     @GetMapping("/")
     public String index(Model model) throws SQLException {
         List<SaveParam> saveParams = javaMockService.queryAllLines();
         model.addAttribute("saveParams", saveParams);
-        return "index"; // 返回模板文件名（不包含.html后缀）
+        return "index";
     }
+
+    @PostMapping("/save")
+    public String configure(@ModelAttribute("saveParam") SaveParam saveParam) throws SQLException {
+        javaMockService.saveConfig(saveParam);
+        return "redirect:/";
+    }
+
+    @GetMapping("/detail/{timeStamp}")
+    public String detail(@PathVariable String timeStamp, Model model) throws SQLException {
+        SaveParam saveParam = javaMockService.queryLine(timeStamp);
+        model.addAttribute("saveParam", saveParam);
+        return "detail";
+    }
+
 }
--- src/main/java/org/ma5d/javamock/service/JavaMockServiceImpl.java
+++ src/main/java/org/ma5d/javamock/service/JavaMockServiceImpl.java
@@ -6,6 +6,7 @@ import org.ma5d.javamock.dto.SaveParam;
 import org.springframework.stereotype.Service;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -19,14 +20,19 @@ public class JavaMockServiceImpl {
 
     public boolean saveConfig(SaveParam saveParam) throws SQLException {
         Connection connection = dataSource.getConnection();
-        //language=SQLite
-        String query = "insert into java_mock(time_stamp, adomain, path_param, response) values(?, ?, ?)";
-        return connection.prepareStatement(query, saveParam.toStringArray()).execute();
+        // language=SQLite
+        String query = "insert into java_mock(time_stamp, adomain, path_param, response) values(?, ?, ?, ?)";
+        PreparedStatement stmt = connection.prepareStatement(query, saveParam.toStringArray());
+        stmt.setString(1, saveParam.getTimeStamp());
+        stmt.setString(2, saveParam.getDomainCom());
+        stmt.setString(3, saveParam.getPathWithParam());
+        stmt.setString(4, saveParam.getResponse());
+        return stmt.execute();
     }
 
     public List<SaveParam> queryAllLines() throws SQLException {
         Connection connection = dataSource.getConnection();
-        //language=SQLite
+        // language=SQLite
         String query = "select time_stamp, adomain, path_param, response from java_mock";
         ResultSet resultSet = connection.prepareStatement(query).executeQuery();
         List<SaveParam> saveParams = new ArrayList<>();
@@ -40,5 +46,21 @@ public class JavaMockServiceImpl {
         }
         connection.close();
         return saveParams;
+    }
+
+    public SaveParam queryLine(String timeStamp) throws SQLException {
+        Connection connection = dataSource.getConnection();
+        // language=SQLite
+        String query = "select time_stamp, adomain, path_param, response from java_mock where time_stamp = ?";
+        ResultSet resultSet = connection.prepareStatement(query, new String[]{timeStamp}).executeQuery();
+        SaveParam saveParam = new SaveParam();
+        while (resultSet.next()) {
+            saveParam.setTimeStamp(resultSet.getString("time_stamp"));
+            saveParam.setDomainCom(resultSet.getString("adomain"));
+            saveParam.setPathWithParam(resultSet.getString("path_param"));
+            saveParam.setResponse(resultSet.getString("response"));
+        }
+        connection.close();
+        return saveParam;
     }
 }
